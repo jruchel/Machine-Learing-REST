12299d81d0977279dc64cedc2cb278da406d6e0b
Jakub Ruchel <jruchel@gmail.com>
2021-04-03 20:54:05 +0200

    Instead of running python scripts in java switched to a python backend in an anaconda environment using flask for communication with java

--- src/main/java/com/jruchel/mlrest/controllers/MLScriptController.java
+++ src/main/java/com/jruchel/mlrest/controllers/MLScriptController.java
@@ -1,41 +1,11 @@
 package com.jruchel.mlrest.controllers;
 
 import com.jruchel.mlrest.security.Controller;
-import com.jruchel.mlrest.security.SecuredMapping;
-import com.jruchel.mlrest.services.PythonService;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
-import java.io.IOException;
-
 
 @RestController
 @CrossOrigin
 public class MLScriptController extends Controller {
 
-    private final PythonService pythonService;
-
-    public MLScriptController(PythonService pythonService) {
-        this.pythonService = pythonService;
-    }
-
-    @SecuredMapping(path = "/script", method = RequestMethod.GET)
-    public String runScript(@PathParam("script") String script) {
-        return getScriptResult(script);
-    }
-
-    @SecuredMapping(path = "/ml", method = RequestMethod.GET)
-    public String runML(@PathParam("message") String message) {
-        return getScriptResult("ml", message);
-    }
-
-    private String getScriptResult(String script, String... args) {
-        try {
-            return pythonService.runScript(script, args);
-        } catch (IOException e) {
-            return e.getMessage();
-        }
-    }
 }
