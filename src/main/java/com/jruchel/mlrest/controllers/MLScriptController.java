97744da60a0ea48d2239874b899fbb7d3bee19c3
Jakub Ruchel <jruchel@gmail.com>
2021-04-03 15:45:22 +0200

    Added an endpoint to test wether passign arguments to python scripts works correctly

--- src/main/java/com/jruchel/mlrest/controllers/MLScriptController.java
+++ src/main/java/com/jruchel/mlrest/controllers/MLScriptController.java
@@ -3,7 +3,6 @@ package com.jruchel.mlrest.controllers;
 import com.jruchel.mlrest.security.Controller;
 import com.jruchel.mlrest.security.SecuredMapping;
 import com.jruchel.mlrest.services.PythonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +15,25 @@ import java.io.IOException;
 @CrossOrigin
 public class MLScriptController extends Controller {
 
-    @Autowired
-    private PythonService pythonService;
+    private final PythonService pythonService;
+
+    public MLScriptController(PythonService pythonService) {
+        this.pythonService = pythonService;
+    }
 
     @SecuredMapping(path = "/script", method = RequestMethod.GET)
     public String runScript(@PathParam("script") String script) {
+        return getScriptResult(script);
+    }
+
+    @SecuredMapping(path = "/ml", method = RequestMethod.GET)
+    public String runML(@PathParam("message") String message) {
+        return getScriptResult("ml", message);
+    }
+
+    private String getScriptResult(String script, String... args) {
         try {
-            return pythonService.runScript(script);
+            return pythonService.runScript(script, args);
         } catch (IOException e) {
             return e.getMessage();
         }
